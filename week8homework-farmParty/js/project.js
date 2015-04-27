6f3f01773e1f331c23a483f687a36707f01a7f28
Eric Hoffman <ewhoffman127@gmail.com>
2015-04-26 20:54:22 -0400

    week 8 homework. Farm Party final draft.

---
+++ week8homework-farmParty/js/project.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	// // var character = "<img src=\"images/rooster_doing_crazy_dance.gif\">";
+
+	// $("character").show().delay(4000).animate({
+	// 	left: "30%"
+	// },1000);
+	
+	$("#tree").show().delay(2000).animate({
+		bottom: "20%"
+	},1000, "easeOutBounce");
+
+	$("#sun").show().delay(2000).animate({
+		top: "5%"
+	},1000);
+
+	$("#rooster2").show().delay(4000).animate({
+		left: "30%"
+	},3000);
+});
